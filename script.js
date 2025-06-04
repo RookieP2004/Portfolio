9a51dc3a5cae6e05afb0db14afa261f3718c812b
Sujal Chandel <116681047+RookieP2004@users.noreply.github.com>
2025-06-04 09:45:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function f1() {
+    var A=document.getElementById("A").value;
+    console.log(A);
+     window.open("https://github.com/RookieP2004", "_blank");
+}
+
+function f2() {
+    var B=document.getElementById("B").value;
+    console.log(B);
+     window.open("https://www.linkedin.com/in/sujal-chandel-704b35302/", "_blank");
+}
+
+function f3() {
+    var C=document.getElementById("C").value;
+    console.log(C);
+     window.open("https://x.com/SujalChand9700", "_blank");
+}
+
+function f4() {
+    var D=document.getElementById("D").value;
+    console.log(D);
+     window.open("https://github.com/RookieP2004", "_blank");
+}
+
+function f5() {
+    var E=document.getElementById("E").value;
+    console.log(E);
+     window.open("https://github.com/RookieP2004", "_blank");
+}
